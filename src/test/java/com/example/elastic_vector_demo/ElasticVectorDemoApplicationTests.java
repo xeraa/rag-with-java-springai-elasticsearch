288046a9c48d302aa83ce7811e9581216c125b82
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-30 17:06:07 -0700

    initial commit

---
+++ src/test/java/com/example/elastic_vector_demo/ElasticVectorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elastic_vector_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticVectorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
